71a61c8374e3e10401a2c8f999817e9236d8980b
tameeshb <g@tameesh.in>
2018-06-20 11:14:00 +0530

    Init + homepage

---
+++ public/assets/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBycKdfDXgvYk6TJUMKzOxefteWg1OKHYQ",
+authDomain: "njack-web.firebaseapp.com",
+databaseURL: "https://njack-web.firebaseio.com",
+projectId: "njack-web",
+storageBucket: "njack-web.appspot.com",
+messagingSenderId: "492601859045"
+};
+firebase.initializeApp(config);
